ea3e1f69f0e54bbbb24b6604d151ca1f1eede31a
Stéphane Manciot <stephane.manciot@gmail.com>
2022-04-08 09:46:30 +0200

    update generic jdbc schema - adding oracle and sqlserver

--- jdbc/src/main/scala/app/softnetwork/persistence/jdbc/query/JdbcSchema.scala
+++ jdbc/src/main/scala/app/softnetwork/persistence/jdbc/query/JdbcSchema.scala
@@ -7,7 +7,9 @@ object JdbcSchema {
 
   sealed trait SchemaType { def schema: String }
   abstract class AbstractSchema(val schema: String) extends SchemaType
-  case object Postgres extends AbstractSchema(schema = "schema/postgres-schema.sql")
-  case object H2 extends AbstractSchema(schema = "schema/h2-schema.sql")
-  case object MySQL extends AbstractSchema(schema = "schema/mysql-schema.sql")
+  case object Postgres extends AbstractSchema(schema = "schema/postgres/postgres-create-schema.sql")
+  case object H2 extends AbstractSchema(schema = "schema/h2/h2-create-schema.sql")
+  case object MySQL extends AbstractSchema(schema = "schema/mysql/mysql-create-schema.sql")
+  case object Oracle extends AbstractSchema(schema = "schema/oracle/oracle-create-schema.sql")
+  case object SQLServer extends AbstractSchema(schema = "schema/sqlserver/sqlserver-create-schema.sql")
 }
