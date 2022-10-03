e67b9c75fdcfb987c2bc1f88d30a8ab6f4424d0f
Stéphane Manciot <stephane.manciot@gmail.com>
2022-10-03 20:50:35 +0200

    upgrade dependencies versions (akka http, akka http json4s, akka management, typesafe config, jackson, logback, slf4j)

--- build.sbt
+++ build.sbt
@@ -30,7 +30,7 @@ ThisBuild / organization := "app.softnetwork"
 
 name := "generic-persistence-api"
 
-ThisBuild / version := "0.2.3.16"
+ThisBuild / version := "0.2.3.17"
 
 ThisBuild / scalaVersion := "2.12.11"
 
--- project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
+++ project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
@@ -2,35 +2,35 @@ package app.softnetwork.sbt.build
 
 object Versions {
 
-  val akka = "2.6.18" //2.6.12 -> 2.6.18
+  val akka = "2.6.20"
 
-  val akkaHttp = "10.1.15" //10.1.11 -> 10.2.7
+  val akkaHttp = "10.2.10" //10.1.15 -> 10.2.10
 
-  val akkaHttpJson4s = "1.33.0" //1.31.0 -> 1.39.2
+  val akkaHttpJson4s = "1.37.0" //1.33.0 -> 1.39.2
 
   val akkaPersistenceJdbc = "3.5.2"
 
-  val akkaHttpSession = "0.5.11"
+  val akkaHttpSession = "0.5.11" // 0.5.11 -> 0.7.0
 
-  val akkaManagement = "1.0.10" // 1.0.10 -> 1.1.2
+  val akkaManagement = "1.1.4" // 1.0.10 -> 1.1.4
 
   val postgresql = "42.2.18" //"9.4.1210"
 
   val scalatest = "3.1.1"
 
-  val typesafeConfig = "1.4.0"
+  val typesafeConfig = "1.4.2"
 
   val kxbmap = "0.4.4"
 
-  val jackson = "2.10.5" //2.10.1 -> 2.11.4
+  val jackson = "2.11.4" //2.10.1 -> 2.11.4
 
   val json4s = "3.6.12" // 3.6.7 -> 3.6.12
 
   val scalaLogging = "3.9.2"
 
-  val logback = "1.1.7"
+  val logback = "1.2.3"
 
-  val slf4j = "1.7.30"
+  val slf4j = "1.7.36"
 
   val log4s = "1.8.2"
 
