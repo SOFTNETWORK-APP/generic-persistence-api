26ef190722ba2ffebe76e8482f5624c35d44cb23
Stéphane Manciot <stephane.manciot@gmail.com>
2023-05-24 22:19:34 +0200

    typo

--- server/src/main/scala/app/softnetwork/api/server/ApiEndpoints.scala
+++ server/src/main/scala/app/softnetwork/api/server/ApiEndpoints.scala
@@ -10,12 +10,12 @@ trait ApiEndpoints extends ApiRoutes {
 
   import ApiEndpoint._
 
-  def endPoints: ActorSystem[_] => List[ServerEndpoint[Any, Future]]
+  def endpoints: ActorSystem[_] => List[ServerEndpoint[Any, Future]]
 
   override def apiRoutes(system: ActorSystem[_]): Route = {
     implicit def ec: ExecutionContext = system.executionContext
-    val routes = endPoints(system)
-    routes ++ endpointsToSwaggerEndpoints(routes)
+    val eps = endpoints(system)
+    eps ++ endpointsToSwaggerEndpoints(eps)
   }
 
 }
