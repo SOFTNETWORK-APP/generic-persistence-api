7d3b4e81f2f0f2de193a3f5cc7b66d368dfb393c
Stéphane Manciot <stephane.manciot@gmail.com>
2023-05-24 17:57:12 +0200

    update api endpoints

--- server/src/main/scala/app/softnetwork/api/server/ApiEndpoints.scala
+++ server/src/main/scala/app/softnetwork/api/server/ApiEndpoints.scala
@@ -2,14 +2,20 @@ package app.softnetwork.api.server
 
 import akka.actor.typed.ActorSystem
 import akka.http.scaladsl.server.Route
+import sttp.tapir.server.ServerEndpoint
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
-trait ApiEndpoints extends ApiEndpoint with ApiRoutes {
+trait ApiEndpoints extends ApiRoutes {
+
+  import ApiEndpoint._
+
+  def endPoints: ActorSystem[_] => List[ServerEndpoint[Any, Future]]
 
   override def apiRoutes(system: ActorSystem[_]): Route = {
     implicit def ec: ExecutionContext = system.executionContext
-    apiRoute ~ swaggerRoute
+    val routes = endPoints(system)
+    routes ++ endpointsToSwaggerEndpoints(routes)
   }
 
 }
