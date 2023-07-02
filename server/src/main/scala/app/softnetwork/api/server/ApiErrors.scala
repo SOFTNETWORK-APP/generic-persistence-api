7c33f7769c9a73358c3dca8240c7a3cd198c347f
Stéphane Manciot <stephane.manciot@gmail.com>
2023-07-02 11:15:19 +0200

    add implicit apiError2Route

--- server/src/main/scala/app/softnetwork/api/server/ApiErrors.scala
+++ server/src/main/scala/app/softnetwork/api/server/ApiErrors.scala
@@ -1,5 +1,10 @@
 package app.softnetwork.api.server
 
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.server.Route
+import org.json4s.Formats
+
 object ApiErrors {
 
   sealed trait ErrorInfo {
@@ -26,4 +31,17 @@ object ApiErrors {
 
   /** Represents http 500. */
   case class InternalServerError(message: String) extends ErrorInfo
+
+  implicit def apiError2Route(apiError: ErrorInfo)(implicit formats: Formats): Route =
+    apiError match {
+      case r: BadRequest => complete(HttpResponse(StatusCodes.BadRequest, entity = r))
+      case r: Conflict   => complete(HttpResponse(StatusCodes.Conflict, entity = r))
+      case r: Forbidden  => complete(HttpResponse(StatusCodes.Forbidden, entity = r))
+      case r: InternalServerError =>
+        complete(HttpResponse(StatusCodes.InternalServerError, entity = r))
+      case r: NotFound     => complete(HttpResponse(StatusCodes.NotFound, entity = r))
+      case r: Unauthorized => complete(HttpResponse(StatusCodes.Unauthorized, entity = r))
+      case r: ErrorMessage => complete(HttpResponse(StatusCodes.OK, entity = r))
+    }
+
 }
