e545b25f5870fda9db3f1958c3c7deee78b43cf9
Stéphane Manciot <stephane.manciot@gmail.com>
2023-07-02 10:26:49 +0200

    update ErrorInfo

--- server/src/main/scala/app/softnetwork/api/server/ApiErrors.scala
+++ server/src/main/scala/app/softnetwork/api/server/ApiErrors.scala
@@ -3,27 +3,27 @@ package app.softnetwork.api.server
 object ApiErrors {
 
   sealed trait ErrorInfo {
-    val msg: String
+    val message: String
   }
 
   /** Represents http 404. */
-  case class NotFound(msg: String) extends ErrorInfo
+  case class NotFound(message: String) extends ErrorInfo
 
   /** Represents http 401. */
-  case class Unauthorized(msg: String) extends ErrorInfo
+  case class Unauthorized(message: String) extends ErrorInfo
 
   /** Represents http 403. */
-  case class Forbidden(msg: String) extends ErrorInfo
+  case class Forbidden(message: String) extends ErrorInfo
 
   /** Represents http 409. */
-  case class Conflict(msg: String) extends ErrorInfo
+  case class Conflict(message: String) extends ErrorInfo
 
   /** Default case. */
-  case class ErrorMessage(msg: String) extends ErrorInfo
+  case class ErrorMessage(message: String) extends ErrorInfo
 
   /** Represents http 400. */
-  case class BadRequest(msg: String) extends ErrorInfo
+  case class BadRequest(message: String) extends ErrorInfo
 
   /** Represents http 500. */
-  case class InternalServerError(msg: String) extends ErrorInfo
+  case class InternalServerError(message: String) extends ErrorInfo
 }
